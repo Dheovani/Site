aff713db92a4a4a2314b21f01f6f088884edbb17
Dheovani Xavier <dheovani_xavier@outlook.com>
2021-10-07 15:20:36 -0300

    Finalizando updates

--- mouse.js
+++ mouse.js
@@ -1,5 +1,5 @@
 mouseOver = function() {
-    if (document.getElementById("theme").href == "lightstyle.css") {
+    if (document.getElementById("theme").href == "https://dheovani.github.io/Site/lightstyle.css") {
         document.getElementById("lamp").src = "icons/lamp-off.png";
     } else {
         document.getElementById("lamp").src = "icons/lamp-dark.png";
@@ -7,7 +7,7 @@ mouseOver = function() {
 }
 
 mouseOut = function() {
-    if (document.getElementById("theme").href == "lightstyle.css") {
+    if (document.getElementById("theme").href == "https://dheovani.github.io/Site/lightstyle.css") {
         document.getElementById("lamp").src = "icons/lamp.png";
     } else {
         document.getElementById("lamp").src = "icons/lamp-dark-off.png";
