e0501ff57fb325af459b48ad20f8d90530f56b3e
Dheovani Xavier <dheovani_xavier@outlook.com>
2021-10-07 14:38:40 -0300

    Teste

--- tema.js
+++ tema.js
@@ -1,34 +1,34 @@
 const docTheme = document.getElementById("theme");
-const lightStyle = "file:///C:/Users/PICHAU/Documents/GitHub/Site/lightstyle.css";
-const darkStyle = "file:///C:/Users/PICHAU/Documents/GitHub/Site/darkstyle.css";
+const lightStyle = "lightstyle.css";
+const darkStyle = "darkstyle.css";
 // Variáveis para salvar o path para os temas lightstyle e darkstyle
 
 function darkIcon() {
-    document.getElementById("me-icon").src = "file:///C:/Users/PICHAU/Documents/GitHub/Site/icons/me-icon.jpeg";
+    document.getElementById("me-icon").src = "icons/me-icon.jpeg";
 
-    document.getElementById("lamp").src = "file:///C:/Users/PICHAU/Documents/GitHub/Site/icons/lamp-dark-off.png";
+    document.getElementById("lamp").src = "icons/lamp-dark-off.png";
 
-    document.getElementById("github").src = "file:///C:/Users/PICHAU/Documents/GitHub/Site/iconesredesocial/github-white.png";
+    document.getElementById("github").src = "iconesredesocial/github-white.png";
 
-    document.getElementById("telegram").src = "file:///C:/Users/PICHAU/Documents/GitHub/Site/iconesredesocial/telegram-white.png";
+    document.getElementById("telegram").src = "iconesredesocial/telegram-white.png";
 
-    document.getElementById("instagram").src = "file:///C:/Users/PICHAU/Documents/GitHub/Site/iconesredesocial/instagram-white.png";
+    document.getElementById("instagram").src = "iconesredesocial/instagram-white.png";
 
-    document.getElementById("linkedin").src = "file:///C:/Users/PICHAU/Documents/GitHub/Site/iconesredesocial/linkedin-white.png";
+    document.getElementById("linkedin").src = "iconesredesocial/linkedin-white.png";
 }
 
 function lightIcon() {
-    document.getElementById("me-icon").src = "file:///C:/Users/PICHAU/Documents/GitHub/Site/icons/me-glasses-icon.jpeg";
+    document.getElementById("me-icon").src = "icons/me-glasses-icon.jpeg";
 
-    document.getElementById("lamp").src = "file:///C:/Users/PICHAU/Documents/GitHub/Site/icons/lamp.png";
+    document.getElementById("lamp").src = "icons/lamp.png";
 
-    document.getElementById("github").src = "file:///C:/Users/PICHAU/Documents/GitHub/Site/iconesredesocial/github.png";
+    document.getElementById("github").src = "iconesredesocial/github.png";
 
-    document.getElementById("telegram").src = "file:///C:/Users/PICHAU/Documents/GitHub/Site/iconesredesocial/telegram.png";
+    document.getElementById("telegram").src = "iconesredesocial/telegram.png";
 
-    document.getElementById("instagram").src = "file:///C:/Users/PICHAU/Documents/GitHub/Site/iconesredesocial/instagram.png";
+    document.getElementById("instagram").src = "iconesredesocial/instagram.png";
 
-    document.getElementById("linkedin").src = "file:///C:/Users/PICHAU/Documents/GitHub/Site/iconesredesocial/linkedin.png";
+    document.getElementById("linkedin").src = "iconesredesocial/linkedin.png";
 }
 
 themeChanger = function () {
